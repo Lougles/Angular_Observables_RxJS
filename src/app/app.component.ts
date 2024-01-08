def7f3a990919560ed624ac3cbc938261c4a68e3
Vova Chelidze <louglescom@gmail.com>
2024-01-08 21:46:30 +0200

    [Section-13-Observables] Subjects unsubscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from "./user.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated: boolean = false;
+  private activatedSub: Subscription;
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.activatedEmiter.subscribe((didActivated: boolean): void => {
+    this.activatedSub = this.userService.activatedEmiter.subscribe((didActivated: boolean): void => {
       this.userActivated = didActivated;
     })
+  }
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
   }
 }
